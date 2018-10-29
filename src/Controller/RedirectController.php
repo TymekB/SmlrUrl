f84ba8ec831a24e5f7cf0097d0cf855b9a4afdf7
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2018-10-29 21:46:49 +0100

    added redirecting to user url

---
+++ src/Controller/RedirectController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ShortUrlRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RedirectController extends AbstractController
+{
+    /**
+     * @var ShortUrlRepository
+     */
+    private $shortUrlRepository;
+
+    public function __construct(ShortUrlRepository $shortUrlRepository)
+    {
+        $this->shortUrlRepository = $shortUrlRepository;
+    }
+
+    public function index($urlId)
+    {
+        $shortUrl = $this->shortUrlRepository->findOneBy(['urlId' => $urlId]);
+
+        if(!$shortUrl) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->redirect($shortUrl->getUrl());
+    }
+}
