96a01afa57ef826d17901c41d208aea5daa3440f
Tymoteusz Baran <baran.tymoteusz@gmail.com>
2018-11-12 10:26:45 +0100

    added deleting option in history

--- public/js/ajax/shortUrl.js
+++ public/js/ajax/shortUrl.js
@@ -1,5 +1,7 @@
 function showAll(DOMElement) {
 
+    console.log('showing all..');
+
     $.ajax({
         method: 'GET',
         url: '/api/short-url',
@@ -12,17 +14,57 @@ function showAll(DOMElement) {
                 let link = window.location.host + "/" + shortUrl.token;
 
                 output += `
-                        <tr>
+                        <tr data-id="${shortUrl.id}">
                             <td>${shortUrl.id}</td>
                             <td>${shortUrl.url}</td>
                             <td><a href="${link}">${link}</a></td>
                             <td>${shortUrl.token}</td>
-                            <td><a href="#" class="btn btn-info btn-sm">Edit</a> <a href="#" class="btn btn-danger btn-sm">Delete</a></td>
+                            <td><button href="#" class="btn btn-info btn-sm">Edit</button> <button href="#" class="btn btn-danger btn-sm delete-action">Delete</a></td>
                         </tr>
                         `;
             });
 
             $(DOMElement).html(output);
+
+            $('.delete-action').click(function() {
+
+                if(!confirm("Are you sure?")) {
+                    return;
+                }
+
+                let id = $(this).parent().parent().data("id");
+
+                remove(id);
+            });
         }
     });
+}
+
+function remove(id) {
+
+    let jsonData = JSON.stringify({id: id});
+
+    $.ajax({
+        method: 'DELETE',
+        data: jsonData,
+        url: '/api/short-url',
+        success: function(data) {
+            $.toast({
+                heading: 'Success!',
+                text: 'Your url has been successfuly deleted!',
+                hideAfter: false,
+                icon: 'success'
+            });
+        },
+        error: function(data) {
+            $.toast({
+                heading: 'Error!',
+                text: 'Something went wrong. Try again later.',
+                hideAfter: false,
+                icon: 'error'
+            });
+        }
+    });
+
+    showAll("#data");
 }
